b59bbea7017c6a30c42f9e9413f5f0cfdc98842a
João Beires <1190718@isep.ipp.pt>
2021-11-09 16:30:31 +0000

    [US101]Test development!

---
+++ src/test/java/lapr/project/controller/ImportFileControllerTest.java
@@ -0,0 +1,28 @@
+package lapr.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImportFileControllerTest {
+
+    @Test
+    void import_ships() {
+    }
+
+    @Test
+    void getTreeOfShips() {
+    }
+
+    @Test
+    void getTreeOfShips2() {
+    }
+
+    @Test
+    void getTreeOfShips3() {
+    }
+
+    @Test
+    void getShipByMMSI() {
+    }
+}
