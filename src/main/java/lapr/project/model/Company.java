76bf54f641d0a7245df29694060fa1974e8697e1
maiad <1191419@isep.ipp.pt>
2021-11-12 18:07:13 +0000

    Testing US107

--- src/main/java/lapr/project/model/Company.java
+++ src/main/java/lapr/project/model/Company.java
@@ -187,7 +187,11 @@ public class Company {
         double distanceDeparture = 0;
         distanceArrival = Distance.distance(a.arrivalLatitude(),a.arrivalLongitude(),b.arrivalLatitude(),b.arrivalLongitude());
         distanceDeparture = Distance.distance(a.departureLatitude(),a.departureLongitude(),b.departureLatitude(),b.departureLongitude());
-        return distanceArrival <= 5 && distanceDeparture <= 5 && a.travelledDistance() >= 10 && b.travelledDistance() >= 10 && a.travelledDistance() != b.travelledDistance();
+        if(distanceArrival <= 5 && distanceDeparture <= 5 && a.travelledDistance() >= 10 && b.travelledDistance() >= 10 && a.travelledDistance() != b.travelledDistance()){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public double travelDistanceDifference(Pair<Ship, Ship> pairOfShips){
