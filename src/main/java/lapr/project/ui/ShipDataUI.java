6d4c0a5276b2da9d13eabd5732ac582b6de98e4d
João Beires <1190718@isep.ipp.pt>
2021-11-13 19:54:51 +0000

    Improved console output for ShipDataUI.java!

--- src/main/java/lapr/project/ui/ShipDataUI.java
+++ src/main/java/lapr/project/ui/ShipDataUI.java
@@ -30,24 +30,25 @@ public class ShipDataUI implements Runnable {
 
             if (ship != null) {
 
+
                 System.out.println("\n\n\t\t\t\t\t\t============================ Ship Data ============================");
 
                 System.out.println("\t\t\t\t\t\tMMSI -> " + ship.getMMSI() +
                         "\n\t\t\t\t\t\tVessel Name -> " + ship.getName() +
                         "\n\t\t\t\t\t\tStart Base Date Time -> " + ship.initialDate() +
                         "\n\t\t\t\t\t\tEnd Base Date Time -> " + ship.finalDate() +
-                        "\n\t\t\t\t\t\tTotal Movement Time -> " + ship.totalMovementTime() +
+                        "\n\t\t\t\t\t\tTotal Movement Time -> " + String.format("%02dh%02dm",(ship.totalMovementTime()/3600),((ship.totalMovementTime() % 3600) / 60)) +
                         "\n\t\t\t\t\t\tTotal Number Of Movements -> " + ship.getTotalMovements() +
-                        "\n\t\t\t\t\t\tMax SOG -> " + ship.getMaxSOG() +
-                        "\n\t\t\t\t\t\tMean SOG -> " + ship.getMeanSOG() +
-                        "\n\t\t\t\t\t\tMax COG -> " + ship.getMaxCOG() +
-                        "\n\t\t\t\t\t\tMean COG -> " + ship.getMeanCOG() +
-                        "\n\t\t\t\t\t\tDeparture Latitude -> " + ship.departureLatitude() +
-                        "\n\t\t\t\t\t\tDeparture Longitude -> " + ship.departureLongitude() +
-                        "\n\t\t\t\t\t\tArrival Latitude -> " + ship.arrivalLatitude() +
-                        "\n\t\t\t\t\t\tArrival Longitude -> " + ship.arrivalLongitude() +
-                        "\n\t\t\t\t\t\tTravelled Distance -> " + ship.travelledDistance() +
-                        "\n\t\t\t\t\t\tDelta Distance -> " + ship.getDeltaDistance()
+                        "\n\t\t\t\t\t\tMax SOG -> " + ship.getMaxSOG() + " km/h" +
+                        "\n\t\t\t\t\t\tMean SOG -> " + String.format("%.3f",ship.getMeanSOG()) + " km/h" +
+                        "\n\t\t\t\t\t\tMax COG -> " + ship.getMaxCOG() + "°" +
+                        "\n\t\t\t\t\t\tMean COG -> " + String.format("%.3f",ship.getMeanCOG()) + "°" +
+                        "\n\t\t\t\t\t\tDeparture Latitude -> " + ship.departureLatitude() + "°" +
+                        "\n\t\t\t\t\t\tDeparture Longitude -> " + ship.departureLongitude() + "°" +
+                        "\n\t\t\t\t\t\tArrival Latitude -> " + ship.arrivalLatitude() + "°" +
+                        "\n\t\t\t\t\t\tArrival Longitude -> " + ship.arrivalLongitude() + "°" +
+                        "\n\t\t\t\t\t\tTravelled Distance -> " + String.format("%.3f" ,ship.travelledDistance()) + " km"+
+                        "\n\t\t\t\t\t\tDelta Distance -> " + String.format("%.3f",ship.getDeltaDistance()) + " km"
                 );
 
             } else {
