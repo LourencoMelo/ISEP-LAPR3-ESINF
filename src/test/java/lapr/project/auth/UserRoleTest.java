76129e6abfb7878393fc9594449f1707e91ff53f
maiad <1191419@isep.ipp.pt>
2021-11-14 15:08:05 +0000

    Improving code smell

--- src/test/java/lapr/project/auth/UserRoleTest.java
+++ src/test/java/lapr/project/auth/UserRoleTest.java
@@ -15,4 +15,30 @@ class UserRoleTest {
         }
     }
 
+    @Test
+    void UserRole2(){
+        try{
+            UserRole role = new UserRole("STUDENT","");
+        }catch (IllegalArgumentException ex){
+            assertEquals(ex.getMessage(),"UserRole id and/or description cannot be blank.");
+        }
+    }
+
+    @Test
+    void getId(){
+        UserRole role = new UserRole("STUDENT","Student of school");
+        String expected = "Student of school";
+        String result = role.getDescription();
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void hasId(){
+        UserRole role = new UserRole("STUDENT","Student of school");
+        boolean result = true;
+        result =  role.hasId("");
+        assertFalse(result);
+    }
+
+
 }
