b7bbe298774f04c7be16eb147d94a1ceb6692f83
João Beires <1190718@isep.ipp.pt>
2021-11-14 19:31:17 +0000

    Resolved issue on AuthControllerTest.java!

--- src/test/java/lapr/project/controller/AuthControllerTest.java
+++ src/test/java/lapr/project/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package lapr.project.controller;
 
+import lapr.project.auth.AuthFacade;
 import lapr.project.utils.App;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,22 @@ class AuthControllerTest {
         AuthController authController = new AuthController();
 
         assertEquals(authController.getApp(), App.getInstance());
+    }
+
+    @Test
+    void doLogOutTest() {
+        AuthController authController = new AuthController();
+
+        AuthFacade authFacade = authController.getApp().getCompany().getAuthFacade();
+
+        authFacade.addUserRole("Traffic Manager", "Traffic Manager");
+
+        authFacade.addUserWithRole("Joao", "joao@gmail.com", "12345", "Traffic Manager");
+
+        authFacade.doLogin("joao@gmail.com", "12345");
+
+        authController.doLogout();
+
+        assertNull(authController.getApp().getCurrentUserSession().getUser());
     }
 }
