322f05541fb717b5156505f907b8e698b5a70080
LourencoMelo <1190811@isep.ipp.pt>
2021-12-04 22:12:00 +0000

    <ISSUE_70><ISSUE_74>: #US204 & #US208 2 Code Smells

--- src/main/java/lapr/project/model/PositionData.java
+++ src/main/java/lapr/project/model/PositionData.java
@@ -1,8 +1,6 @@
 package lapr.project.model;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Objects;
 
 public class PositionData implements Comparable<PositionData> {
 
